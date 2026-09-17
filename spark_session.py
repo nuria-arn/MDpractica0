9fb7daef73f0999008bf7b168409166c06152d0c
Nuria Garcia <ngarca08@estudiantes.unileon.es>
2026-09-17 14:53:24 +0200

    Meter csv en el repositorio

--- spark_session.py
+++ spark_session.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from pyspark.sql.window import *
 
-spark = SparkSession.builder.getOrCreate()
+spark_session = (SparkSession.builder.appName("IBEX35").getOrCreate())
