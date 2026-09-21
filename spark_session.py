5e316e49436c0643af9fe46da1b69d07754d6b73
Nuria Garcia <ngarca08@estudiantes.unileon.es>
2026-09-21 23:53:26 +0200

    Cambio nombre archivo

--- spark_session.py
+++ spark_session.py
@@ -4,3 +4,4 @@ from pyspark.sql.types import *
 from pyspark.sql.window import *
 
 spark_session = (SparkSession.builder.appName("IBEX35").getOrCreate())
+df = spark_session.read.option("header", True).option("sep", ";").option("dateFormat", "dd/MM/yyyy").csv("ibex35_close_2024.csv")
