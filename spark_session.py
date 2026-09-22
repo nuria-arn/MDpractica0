0ec8d30117ef2bd1805a21d6fb9cd6761a9d32a3
Nuria Garcia <ngarca08@estudiantes.unileon.es>
2026-09-22 12:48:53 +0200

    Ejercicio 1-b

--- spark_session.py
+++ spark_session.py
@@ -14,7 +14,16 @@ df.printSchema()
 
 # print("========================================================================================================================")
 
-df_new = df.withColumn("Fecha", to_date(col("Fecha"), "dd/MM/yyyy"))
+df_new = df.withColumn("Fecha", regexp_replace(col("Fecha"), r"(\d{2})/(\d{2})/(\d{4})", r"$3-$2-$1"))
 df_new = df_new.withColumn("Fecha", col("Fecha").cast("date"))
 df_new.printSchema()
 print(df_new.show(6))
+
+# Ej1-b Eliminar sufijo .MC de los nombres de las columnas
+
+df = df_new
+for i in df.columns:
+    if ".MC" in i:
+        df = df.withColumnRenamed(i, i[:-3])
+
+print(df.columns)
