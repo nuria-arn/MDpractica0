ecc912e5fc431e5eb0ce63006d983b9671c84590
Nuria Garcia <ngarca08@estudiantes.unileon.es>
2026-09-22 12:06:15 +0200

    Ejercicio 1-a

--- spark_session.py
+++ spark_session.py
@@ -5,8 +5,16 @@ from pyspark.sql.window import *
 
 spark_session = (SparkSession.builder.appName("IBEX35").getOrCreate())
 df = spark_session.read.option("header", True).option("sep", ";").option("dateFormat", "dd/MM/yyyy").csv("ibex35_close_2024.csv")
-print("========================================================================================================================")
-print(df.columns)
-print("========================================================================================================================")
-primerosSeis = df.head(6)[0]
-print(primerosSeis)
+
+# Ej1-a Mostrar esquema de tipos y convertir columna "Fecha" 
+# Mostrar nuevo esquema y 6 filas del DataFrame
+
+print("Ej1-a")
+df.printSchema()
+
+# print("========================================================================================================================")
+
+df_new = df.withColumn("Fecha", to_date(col("Fecha"), "dd/MM/yyyy"))
+df_new = df_new.withColumn("Fecha", col("Fecha").cast("date"))
+df_new.printSchema()
+print(df_new.show(6))
