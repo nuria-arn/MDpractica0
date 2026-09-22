d90b8f616b108cb2a40071723edb8f276dad9d84
Nuria Garcia <ngarca08@estudiantes.unileon.es>
2026-09-22 11:23:08 +0200

    Imprimir columnas

--- spark_session.py
+++ spark_session.py
@@ -5,3 +5,8 @@ from pyspark.sql.window import *
 
 spark_session = (SparkSession.builder.appName("IBEX35").getOrCreate())
 df = spark_session.read.option("header", True).option("sep", ";").option("dateFormat", "dd/MM/yyyy").csv("ibex35_close_2024.csv")
+print("========================================================================================================================")
+print(df.columns)
+print("========================================================================================================================")
+primerosSeis = df.head(6)[0]
+print(primerosSeis)
